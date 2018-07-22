e1e69cb905ce19a64ad2ac0d496eb77deec7c362
Gunther Cox <gunthercx@gmail.com>
2018-07-22 16:17:40 -0400

    Replace Conversation model with field

--- chatterbot/ext/django_chatterbot/models.py
+++ chatterbot/ext/django_chatterbot/models.py
@@ -1,6 +1,5 @@
 from chatterbot.ext.django_chatterbot.abstract_models import (
-    AbstractBaseConversation, AbstractBaseResponse,
-    AbstractBaseStatement, AbstractBaseTag
+    AbstractBaseStatement, AbstractBaseResponse, AbstractBaseTag
 )
 
 
@@ -16,13 +15,6 @@ class Response(AbstractBaseResponse):
     """
     A connection between a statement and anther statement
     that response to it.
-    """
-    pass
-
-
-class Conversation(AbstractBaseConversation):
-    """
-    A sequence of statements representing a conversation.
     """
     pass
 
