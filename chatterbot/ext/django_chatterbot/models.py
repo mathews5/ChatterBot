9f9d169cd4cc496814f986367b4e186e4bfc6b10
Gunther Cox <gunthercx@gmail.com>
2018-09-09 14:45:39 -0400

    temp

--- chatterbot/ext/django_chatterbot/models.py
+++ chatterbot/ext/django_chatterbot/models.py
@@ -1,5 +1,5 @@
 from chatterbot.ext.django_chatterbot.abstract_models import (
-    AbstractBaseStatement, AbstractBaseResponse, AbstractBaseTag
+    AbstractBaseStatement, AbstractBaseTag
 )
 
 
@@ -7,14 +7,6 @@ class Statement(AbstractBaseStatement):
     """
     A statement represents a single spoken entity, sentence or
     phrase that someone can say.
-    """
-    pass
-
-
-class Response(AbstractBaseResponse):
-    """
-    A connection between a statement and anther statement
-    that response to it.
     """
     pass
 
